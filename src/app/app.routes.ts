416f2feca07bdc0a12c136370b459667961c92f3
Salvatore Celano <salvatore.celano@ennova-research.com>
2024-06-25 10:57:20 +0200

    init: init project with moudele angualrMaterial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TableComponent } from './components/table/table.component';
+import { UserComponent } from './components/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: "", component: HomeComponent },
+{ path: "table", component: TableComponent },
+{ path: "user", component: UserComponent },
+];
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { AngularMaterialModule } from '../../modules/material.module';
+
+@Component({
+  standalone: true,
+  imports: [ AngularMaterialModule ],
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+})
+export class NavbarComponent {
+
+}
